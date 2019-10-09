7d5743b4e1e68414fc098213b1e10b5c590f0ba3
Tony Xiang <tony.xiang@alliander.com>
2019-10-09 15:31:29 +0200

    try eigen3

--- try_xtensor/three_phase_tensor.hpp
+++ try_xtensor/three_phase_tensor.hpp
@@ -1,10 +1,39 @@
 #pragma once
 
 #include <cmath>
-#include "xtensor/xfixed.hpp"
+#include <cstddef>
+#include <type_traits>
+#include <complex>
+#include "Eigen/Dense"
 
 namespace three_phase_tensor {
 
+using DoubleComplex = std::complex<double>;
+
+template<class T>
+using Vector3P = Eigen::Array<T, 3, 1, Eigen::DontAlign>;
+
+template<class T>
+using Tensor3P = Eigen::Array<T, 3, 3, Eigen::RowMajor | Eigen::DontAlign>;
+
+template<class T>
+using Diag3P = Eigen::DiagonalMatrix<T, 3>;
+
+inline double tensor_vector_mult(double x, double y) {
+	return x * y;
+}
+
+template<class T>
+inline auto tensor_vector_mult(Tensor3P<T> x, Vector3P<T> y) {
+	return (x.matrix() * y.matrix()).array();
+}
+
+template<bool sym>
+using ComplexTensor = std::conditional_t<sym, DoubleComplex, Tensor3P<DoubleComplex>>;
+template<bool sym>
+using RealValue = std::conditional_t<sym, double, Vector3P<double>>;
+template<bool sym>
+using ComplexValue = std::conditional_t<sym, DoubleComplex, Vector3P<DoubleComplex>>;
 
 
 }
--- try_xtensor/try_xtensor.cpp
+++ try_xtensor/try_xtensor.cpp
@@ -2,19 +2,21 @@
 //
 
 #include <iostream>
+#include "three_phase_tensor.hpp"
+
 
 int main()
 {
+	using namespace three_phase_tensor;
+
+	ComplexValue<false> a;
+	a << 1, 2, 3;
+	ComplexTensor<false> b;
+	b << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+
+	ComplexValue<false> c = tensor_vector_mult(b, a);
+
+	std::cout << c << '\n';
+
     std::cout << "Hello World!\n"; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
