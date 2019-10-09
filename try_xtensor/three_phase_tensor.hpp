2b3de7d82202461e24280f2ed49236788cbf7299
Tony Xiang <tony.xiang@alliander.com>
2019-10-09 16:31:15 +0200

    use matrix class

--- try_xtensor/three_phase_tensor.hpp
+++ try_xtensor/three_phase_tensor.hpp
@@ -11,34 +11,36 @@ namespace three_phase_tensor {
 using DoubleComplex = std::complex<double>;
 
 template<class T>
-using Vector3P = Eigen::Array<T, 3, 1, Eigen::DontAlign>;
+using Vector3P = Eigen::Matrix<T, 3, 1, Eigen::DontAlign>;
 
 template<class T>
-using Tensor3P = Eigen::Array<T, 3, 3, Eigen::RowMajor | Eigen::DontAlign>;
+using Tensor3P = Eigen::Matrix<T, 3, 3, Eigen::RowMajor | Eigen::DontAlign>;
 
 template<class T>
 using Diag3P = Eigen::DiagonalMatrix<T, 3>;
 
-inline double tensor_vector_mult(double x, double y) {
-	return x * y;
-}
-template<class T>
-inline auto tensor_vector_mult(Tensor3P<T> x, Vector3P<T> y) {
-	return (x.matrix() * y.matrix()).array();
-}
 
 inline double vector_kron_product(double x, double y) {
 	return x * y;
 }
 template<class T>
 inline auto vector_kron_product(Vector3P<T> x, Vector3P<T> y) {
-	Tensor3P<T> z;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			z(i, j) = x(i) * y(j);
-	return z;
+	return x * y.transpose();
 }
 
+inline double ele_mult(double x, double y) {
+	return x * y;
+}
+inline DoubleComplex ele_mult(DoubleComplex x, DoubleComplex y) {
+	return x * y;
+}
+template<class T>
+inline auto ele_mult(Vector3P<T> x, Vector3P<T> y) {
+	return x.cwiseProduct(y);
+}
+
+template<bool sym>
+using RealTensor = std::conditional_t<sym, double, Tensor3P<double>>;
 template<bool sym>
 using ComplexTensor = std::conditional_t<sym, DoubleComplex, Tensor3P<DoubleComplex>>;
 template<bool sym>
--- try_xtensor/try_xtensor.cpp
+++ try_xtensor/try_xtensor.cpp
@@ -9,17 +9,19 @@ int main()
 {
 	using namespace three_phase_tensor;
 
-	ComplexValue<false> a;
+	RealValue<false> a;
 	a << 1, 2, 3;
-	ComplexValue<false> a1;
+	RealValue<false> a1;
 	a1 << 4, 5, 6;
-	ComplexTensor<false> b;
+	RealTensor<false> b;
 	b << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 
-	ComplexValue<false> c = tensor_vector_mult(b, a);
-	ComplexTensor<false> d = vector_kron_product(a, a1);
+	RealValue<false> c = b * a;
+	RealTensor<false> d = vector_kron_product(a, a1);
+	RealValue<false> e = ele_mult(a, a1) + a;
 
 	std::cout << d << '\n';
+	std::cout << e << '\n';
 
     std::cout << "Hello World!\n"; 
 }
